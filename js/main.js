3edf387f1076796949b936b518d61b97633909c3
Peter Li <lipeter2006@gmail.com>
2017-11-20 08:29:16 -0800

    created game logic

---
+++ js/main.js
@@ -0,0 +1,8 @@
+console.log("here's Johnny!");
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+console.log("User Flipped" + cardOne);
+console.log("User Flipped" + cardThree);
