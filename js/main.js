8ea9292886755be5a14c40d3ff124835e53c787c
peterpickles <lipeter2006@gmail.com>
2017-11-22 09:48:08 -0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
-console.log("here's Johnny!");
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);
+
+//Per instructions from assignment 8
+/*var cards = [
+	"queen",
+	"queen",
+	"king",
+	"king",
+	'cardOne'
+]
+
+var cardsInPlay = [
+	push (cardOne)
+]
+
 console.log("User Flipped" + cardOne);
-console.log("User Flipped" + cardThree);
+console.log("User Flipped" + cardThree);*/
