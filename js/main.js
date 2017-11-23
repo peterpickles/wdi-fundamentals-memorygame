5cec2a1809651c25c1e9b47ba776e49b945bad02
peterpickles <lipeter2006@gmail.com>
2017-11-22 16:54:09 -0800

    created more game logic

--- js/main.js
+++ js/main.js
@@ -1,23 +1,22 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ['queen','queen','king','king']; //create an array to store all cards
+
+var cardsInPlay = []; //empty array
+
+var cardOne = cards[0];  //add queen card to array
+
+cardsInPlay.push(cardOne); 
+
+var cardTwo = cards[2]; //add king card to the array
+
+cardsInPlay.push(cardTwo);
 
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+console.log("User flipped " + cardTwo);
 
-//Per instructions from assignment 8
-/*var cards = [
-	"queen",
-	"queen",
-	"king",
-	"king",
-	'cardOne'
-]
-
-var cardsInPlay = [
-	push (cardOne)
-]
-
-console.log("User Flipped" + cardOne);
-console.log("User Flipped" + cardThree);*/
+if (cardsInPlay.length ===2) { //see if two cards are in the array
+	if (cardsInPlay[0] === cardsInPlay[1]) { //if the two cards in the array is a match
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again!")
+	}
+}
